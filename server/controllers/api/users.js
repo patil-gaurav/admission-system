eddffe762570ac42df498ef15c9f61f886975385
Gaurav Patil <gpatil@grepruby.com>
2018-07-03 18:29:35 +0530

    Changed the simple-jwt to jsonwebtoken

--- server/controllers/api/users.js
+++ server/controllers/api/users.js
@@ -3,6 +3,7 @@ var User = require('../../models/user');
 // var passport = require('passport');
 // var bcrypt = require('bcrypt');
 var jwt = require('jwt-simple');
+var jsonwebtoken = require('jsonwebtoken');
 var config = require('../../../config/database');
 
 module.exports = {
@@ -36,7 +37,15 @@ module.exports = {
             } else {
                 user.comparePassword(req.body.password, function(err, isMatch) {
                     if (isMatch && !err) {
-                        var token = jwt.encode(user, config.secret);
+                        // Using simple-jwt
+                        // var token = jwt.encode(user, config.secret);
+
+
+                        // Using jsonwebtoken
+                        var token = jsonwebtoken.sign({email: user.email}, config.secret, {
+                            expiresIn: 15 // Expires in 60 seconds
+                        });
+
                         res.json({success: true, token: 'JWT ' + token});
                     } else {
                         res.json({success: false, msg: 'Authentication faild, wrong password.'});
@@ -50,19 +59,29 @@ module.exports = {
     me(req, res, next) {
         var token = getToken(req.headers);
         if (token) {
-            var decoded = jwt.decode(token, config.secret);
-            User.findOne({
-                email: decoded.email
-            }, function(err, user) {
+            // Using simple-jwt
+            // var decoded = jwt.decode(token, config.secret);
+
+            // User.findOne({
+            //     email: decoded.email
+            // }, function(err, user) {
+            //     if (err) {
+            //         return res.send({success: false, msg: 'Token not valid'});
+            //     }
+            //     if (!user) {
+            //         return res.status(403).send({success: false, msg: 'Authentication failed, User not found'});
+            //     } else {
+            //         res.json({success: true, msg: 'Welcome to my area ' + user.email + '!'});
+            //     }
+            // });
+
+            // Using jsonwebtoken
+            jsonwebtoken.verify(token, config.secret, function(err, decoded) {
                 if (err) {
-                    return res.send({success: false, msg: 'Token not valid'});
+                    return res.status(401).send({success: false, msg: 'Failed to authenticate token'})
                 }
-                if (!user) {
-                    return res.status(403).send({success: false, msg: 'Authentication failed, User not found'});
-                } else {
-                    res.json({success: true, msg: 'Welcome to my area ' + user.email + '!'});
-                }
-            });
+                res.status(200).send(decoded);
+            })
         } else {
             return res.status(403).send({success: false, msg: 'Token not provided'});
         }
